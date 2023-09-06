315fc0ca7599e154f2b6889c754f49a5b33e90bd
BigFatDuck1 <clementho999@gmail.com>
2023-09-06 18:39:26 +0800

    Can generate specified number of divs

--- script.js
+++ script.js
@@ -2,20 +2,24 @@
 
 let div_container = document.querySelector(".div_container");
 
-let number_of_divs = 16;
+let number_of_rows = 16;
+let number_of_columns = 16;
 
-let createDivs = () => {
-
-    console.log(div_container)
-
-    //Loop specified number of times
-    for (let i = 0; i < number_of_divs; i++) {
+let generateOneColumn = (dimensionY) => {
+    for (let i = 0; i < dimensionY; i++) {
         let div_child = document.createElement("div");
         div_child.classList.add("div_child");
         div_container.appendChild(div_child);
-        console.log(i)
+    }
+}
+
+let createDivs = (dimensionX, dimensionY) => {
+
+    //Loop specified number of times
+    for (let i = 0; i < dimensionX; i++) { //dimensionX is number of columns
+        generateOneColumn(dimensionY);
     }
 
 }
 
-createDivs();
+createDivs(number_of_columns, number_of_rows);
